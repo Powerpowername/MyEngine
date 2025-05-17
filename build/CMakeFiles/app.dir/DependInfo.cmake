f60bfd688440136a8d75d59242f841c66990c7b4
Powerpowername <1361581236@qq.com>
2025-05-17 19:06:37 +0800

    初步实现可执行程序，任存在bug需要调整

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/lib_c/glad/src/glad.c" "CMakeFiles/app.dir/E_/lib_c/glad/src/glad.c.obj" "gcc" "CMakeFiles/app.dir/E_/lib_c/glad/src/glad.c.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/All.cpp" "CMakeFiles/app.dir/source/All.cpp.obj" "gcc" "CMakeFiles/app.dir/source/All.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/imgui.cpp" "CMakeFiles/app.dir/source/imgui.cpp.obj" "gcc" "CMakeFiles/app.dir/source/imgui.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/imgui_draw.cpp" "CMakeFiles/app.dir/source/imgui_draw.cpp.obj" "gcc" "CMakeFiles/app.dir/source/imgui_draw.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/imgui_impl_glfw.cpp" "CMakeFiles/app.dir/source/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/app.dir/source/imgui_impl_glfw.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/imgui_impl_opengl3.cpp" "CMakeFiles/app.dir/source/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/app.dir/source/imgui_impl_opengl3.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/imgui_tables.cpp" "CMakeFiles/app.dir/source/imgui_tables.cpp.obj" "gcc" "CMakeFiles/app.dir/source/imgui_tables.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/imgui_widgets.cpp" "CMakeFiles/app.dir/source/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/app.dir/source/imgui_widgets.cpp.obj.d"
+  "E:/GitStore/MyEngine/MyEngine/source/main.cpp" "CMakeFiles/app.dir/source/main.cpp.obj" "gcc" "CMakeFiles/app.dir/source/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/E_/lib_c/glad/src/glad.c.obj"
+  "CMakeFiles/app.dir/E_/lib_c/glad/src/glad.c.obj.d"
+  "CMakeFiles/app.dir/source/All.cpp.obj"
+  "CMakeFiles/app.dir/source/All.cpp.obj.d"
+  "CMakeFiles/app.dir/source/imgui.cpp.obj"
+  "CMakeFiles/app.dir/source/imgui.cpp.obj.d"
+  "CMakeFiles/app.dir/source/imgui_draw.cpp.obj"
+  "CMakeFiles/app.dir/source/imgui_draw.cpp.obj.d"
+  "CMakeFiles/app.dir/source/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/app.dir/source/imgui_impl_glfw.cpp.obj.d"
+  "CMakeFiles/app.dir/source/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/app.dir/source/imgui_impl_opengl3.cpp.obj.d"
+  "CMakeFiles/app.dir/source/imgui_tables.cpp.obj"
+  "CMakeFiles/app.dir/source/imgui_tables.cpp.obj.d"
+  "CMakeFiles/app.dir/source/imgui_widgets.cpp.obj"
+  "CMakeFiles/app.dir/source/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/app.dir/source/main.cpp.obj"
+  "CMakeFiles/app.dir/source/main.cpp.obj.d"
+  "bin/app.exe"
+  "bin/app.exe.manifest"
+  "bin/app.pdb"
+  "libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
