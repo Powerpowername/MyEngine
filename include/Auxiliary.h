a1811026a2e6de74facce26ddd55df8ceea17d1b
Powerpowername <1361581236@qq.com>
2025-05-20 20:38:40 +0800

    修改自建引擎的设计架构

---
+++ include/Auxiliary.h
@@ -0,0 +1,232 @@
+#pragma once
+
+#define IMGUI_DEFINE_MATH_OPERATORS // 必须在 #include "imgui.h" 之前
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD // 告诉 ImGui 使用 GLAD
+#define GLM_ENABLE_EXPERIMENTAL 
+#pragma region 预处理
+#define GLEW_STATIC
+#define var auto
+#include <iostream>
+#include <stdio.h>
+#include <list>
+#include <vector>
+#include <string>
+#include "imgui.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+using glm::vec2;using glm::vec3;using glm::vec4;using glm::mat3;using glm::mat4;
+using glm::radians;
+using std::vector;
+using std::list;
+using std::string;
+#pragma region 前向声明
+class Object;
+class MonoBehavior;
+class GameObject;
+class Camera;
+class CameraMove;
+class Transform;
+class Input;
+class Setting;
+class AbstractLight;
+#pragma endregion
+
+#pragma region 外部变量
+extern GLFWwindow* window;
+#pragma endregion
+
+
+#pragma region Object
+class Object
+{
+public:
+    string name;
+
+
+    //function
+    Object(string name) : name(name){};
+    virtual void OnGUI() = 0;
+    virtual ~Object();//纯虚函数不可以有定义，但是纯虚析构函数是必须要有定义的，因为没有定义的话，子类在析构时就有可能无法正确析构父类的数据,而且纯虚函数必须在类外实现
+};
+#pragma endregion
+
+#pragma region MonoBehavior:Object
+class MonoBehavior : public Object
+{
+public:
+    bool enable = true;
+    GameObject* gameObject;//所有的抽象行为类都必须归属于一个可操作的对象
+
+    //function
+    MonoBehavior();
+    virtual ~MonoBehavior() override;
+    virtual void Update();//更新自身数据
+    virtual void OnGUI() override;
+};
+#pragma endregion
+
+#pragma region Camera:MonoBehavior
+class Camera : public MonoBehavior
+{
+public:
+    vec4 viewPort;//摄像机视口，渲染到窗口的区域
+    float angle;//摄像机的视野角度
+    float near;//近平面
+    float far;//远平面
+    Transform* transform;//摄像机的坐标变换
+    //将之前从全局定义的移动到了Camra类内,这样设计是为了在Setting类中可以直接修改MainCamera
+    //还有一点需要考虑，如果要做阴影是不是放在Transform里更好
+    mat4 viewMat;
+    mat4 projMat;
+
+
+
+    //function
+    Camera();
+    virtual ~Camera() override;
+    virtual void Update() override;
+    virtual void OnGUI() override;
+};
+#pragma endregion
+
+#pragma region CameraMove:MonoBehavior
+class CameraMove : public MonoBehavior
+{
+public:
+    float currentSpeed = 1.0f;
+    Transform* transform;
+	float sencity = 0.006f;
+	float normalSpeed = 5.0f;
+	float highSpeed = 15.0f;
+
+    //function
+    virtual void Update() override;
+	virtual void OnGUI() override;
+	CameraMove();
+	virtual ~CameraMove()override;
+};
+#pragma endregion
+
+#pragma region Transform:MonoBehavior
+//现在的代码思路给我的感觉是Transform必须是最后一个更新的
+//不是最后一个更新的会差一帧，影响应该不大
+class Transform : public MonoBehavior
+{
+public:
+    //Transform::Update()修改
+    vec3 Forward;
+    vec3 Right;
+    vec3 Up;
+
+public:
+    //由Camera::Update()修改，其他操作对象，按需修改
+	vec3 position = vec3(0, 0, 0);//对象的位置
+	vec3 rotation = vec3(0, 0, 0);//旋转角度，x为轴的旋转角度，以此类推
+	vec3 scale = vec3(1, 1, 1);//对象的缩放比列
+
+    //由CameraMove::Update()修改，其他操作对象，按需修改
+    vec3 WorldUp = vec3(0, 1.0f, 0);
+	float Pitch = .0f;
+	float Yaw = .0f;
+
+	void Translate(vec3 movement);//位移
+    mat4 GetModelMaterix(mat4 world = mat4(1.0f))const;//获取对象model矩阵，主要是用来将对象转换到世界坐标系用的
+
+    virtual void OnGUI() override;
+    virtual void Update() override;//更新
+    Transform(vec3 pos = vec3(0, 0, 0), vec3 rotation = vec3(0, 0, 0), vec3 scanle = vec3(1, 1, 1));
+    virtual ~Transform() override;
+};
+#pragma endregion
+
+#pragma region GameObject:Object
+class GameObject : public Object
+{
+public:
+    int id;//全局唯一ID
+	enum Type
+	{
+		Empty,
+		Cameras,
+		Directional,
+		Spot,
+		Point,
+		Box,
+		Model
+	};
+    GameObject* Parent;//暂定保留
+    list<MonoBehavior*>* scripts;//控制的行为脚本
+
+    //function
+    Transform* transform() const;
+    bool enable = true;
+    void OnGUI()override;
+
+    template<typename T>
+	T* AddComponentStart();
+	template<typename T>
+	T * AddComponentNoStart();
+	template<typename T>
+	T* AddComponentStart(MonoBehavior* component);
+	template<typename T>
+	T* GetComponent()const;
+	template<typename T>
+	void RemoveComponent();
+
+    GameObject(string name, Type type);
+	GameObject(string name, string modelName, string shaderSign);
+	~GameObject();
+};
+#pragma endregion
+
+#pragma region Input
+
+#define KEYS 100
+#define W_ 0
+#define A_ 1
+#define S_ 2
+#define D_ 3
+#define Q_ 4
+#define E_ 5
+#define Shift_ 6
+#define Space_ 7
+
+class Input 
+{
+public:
+	static bool* lastKey;
+	static bool* Key;
+	static bool* keyDown;
+	static bool* keyUp;
+	static glm::vec2 mouseMentDelta;
+	static bool GetKeyDown(int key);
+	static bool GetKey(int key);
+	static bool GetKeyUp(int key);
+	static void GetInput();
+	static void ClearInputEveryFrame();
+	static void InitInput();
+};
+#pragma endregion
+
+#pragma region Setting
+class Setting
+{
+public:
+    const static string settingDir;//暂定为shader的配置目录等，后面改
+    static Camera* MainCamera;
+	static std::vector<AbstractLight*>* lights;
+	static std::list<GameObject*>* gameObjects;
+	static vec2 windowSize;
+	static float deltaTime;
+
+    
+	static bool lockMouse;
+
+	static int LightCount(AbstractLight::LightType type);
+
+
+	static void InitSettings();
+};
+#pragma endregion
---
+++ source/Auxiliary.cpp
@@ -0,0 +1,144 @@
+#include "Auxiliary.h"
+
+Object::~Object()
+{
+}
+//子类会自动调用父类的析构函数
+MonoBehavior::MonoBehavior() : Object("MonoBehavior_")
+{
+    this->gameObject = nullptr;
+}
+
+MonoBehavior::~MonoBehavior()
+{
+
+}
+
+void MonoBehavior::Update()
+{
+}
+
+void MonoBehavior::OnGUI()
+{
+    ImGui::Checkbox("Enable",(bool*)&enable);
+}
+
+Camera::Camera()
+{
+    name += "Camera";
+
+    transform = gameObject->transform();//GameObject在他继承得MonoBehavior中
+	transform->Pitch = radians(15.0f);
+	transform->Yaw = radians(180.0f);
+	viewPort = vec4(0, 0, pWindowSize->x, pWindowSize->y);//暂时不修改，看看后期有没有优化的办法，放到Setting里是不是更好
+}
+
+void Camera::Update()
+{
+    glViewport(viewPort.x, viewPort.y, viewPort.z, viewPort.w);//渲染的窗口
+	viewMat = lookAt(transform->position, transform->position + transform->Forward, transform->WorldUp);
+	projMat = glm::perspective(radians(this->angle), viewPort.z / viewPort.w, near, far);
+}
+//这边的考虑是将修改值得操作和更新Camera本身得属性分离开
+void Camera::OnGUI()
+{
+    MonoBehavior::OnGUI();//有Enable的checkbox要用到
+	ImGui::DragFloat4(("viewPort" + std::to_string(gameObject->id)).c_str(), (float*)&viewPort, 10, 0, 2000);
+	ImGui::DragFloat(("viewAngle" + std::to_string(gameObject->id)).c_str(), (float*)&angle, 3, 0, 180.0f);
+	ImGui::DragFloat(("near" + std::to_string(gameObject->id)).c_str(), (float*)&near, 0.01f, 0, 10);
+	ImGui::DragFloat(("far" + std::to_string(gameObject->id)).c_str(), (float*)&far, 1.0f, 0, 1000);
+}
+
+CameraMove::CameraMove()
+{
+	name += "CameraMove";
+    transform = gameObject->transform();
+}
+
+void CameraMove::Update()
+{
+	MonoBehavior::Update();
+    //摄像机移动速度
+	if (Input::GetKeyDown(Shift_))
+		currentSpeed = highSpeed;
+	if (Input::GetKeyUp(Shift_))
+		currentSpeed = normalSpeed;
+
+    //键盘输出控制
+    if(Input::GetKey(S_))
+        transform->Translate(-currentSpeed * transform->Forward*Setting::deltaTime);//Translate的参数是矢量
+	if (Input::GetKey(W_))
+		transform->Translate(currentSpeed * transform->Forward*Setting::deltaTime);
+	if (Input::GetKey(A_))
+		transform->Translate(-currentSpeed * transform->Right*Setting::deltaTime);
+	if (Input::GetKey(D_))	
+		transform->Translate(currentSpeed*transform->Right*Setting::deltaTime);
+	if (Input::GetKey(Q_))
+		transform->Translate(-currentSpeed * transform->Up*Setting::deltaTime);
+	if (Input::GetKey(E_))
+		transform->Translate(currentSpeed*transform->Up*Setting::deltaTime);
+
+    //是否锁定鼠标，就是看不到鼠标了
+    if (Input::GetKeyDown(Space_))
+	{
+		Setting::lockMouse = !Setting::lockMouse;
+		if (Setting::lockMouse)
+			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		else
+			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
+}
+
+void CameraMove::Update()
+{
+    //没有锁定鼠标就返回，只有锁定鼠标后才能更改Camera的Pitch和Yaw
+    if (!Setting::lockMouse)
+        return;
+	transform->Pitch -= Input::mouseMentDelta.y * sencity;
+	transform->Yaw -= Input::mouseMentDelta.x * sencity;
+}
+
+void CameraMove::OnGUI()
+{
+    //与camera的GUI同理先修改，再更新
+	MonoBehavior::OnGUI();
+	ImGui::DragFloat("sencity", (float*)&sencity, 0.001f, 0, 0.010f);
+	ImGui::DragFloat("normalSpeed", (float*)&normalSpeed, 1, 1, 20);
+	ImGui::DragFloat("highSpeed", (float*)&highSpeed, 1, 5, 50);
+}
+
+CameraMove::~CameraMove()
+{
+}
+#pragma region Transform:MonoBehavior
+void Transform::Translate(vec3 movement)
+{
+}
+#pragma endregion
+
+#pragma region Transform : MonoBehavior
+void Transform::Translate(vec3 movement)
+{
+    position += movement;
+}
+
+mat4 Transform::GetModelMaterix(mat4 world = mat4(1.0f)) const
+{
+    auto model = glm::translate(world,position);
+    model = glm::scale(model,scale);
+	model = rotate(model, radians(rotation.x), vec3(1, 0, 0));
+	model = rotate(model, radians(rotation.y), vec3(0, 1, 0));
+	model = rotate(model, radians(rotation.z), vec3(0, 0, 1));
+	return model;
+}
+
+void Transform::OnGUI()
+{
+    MonoBehavior::OnGUI();
+	ImGui::DragFloat3((gameObject->name + "_position").c_str(), (float*)&position, 0.5f, -500, 500);
+	ImGui::DragFloat3((gameObject->name + "_rotation").c_str(), (float*)&rotation, 0.5f, -500, 500);
+	ImGui::DragFloat3((gameObject->name + "_scale").c_str(), (float*)&scale, 0.01f, -100, 100);
+	ImGui::DragFloat((gameObject->name + "_Yaw").c_str(), (float*)&Yaw, 0.01f, -10, 10);
+	ImGui::DragFloat((gameObject->name + "_Pitch").c_str(), (float*)&Pitch, 0.01f, -10, 10);
+}
+#pragma region
